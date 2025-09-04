62a56d6b908f204eeea2e15fd6f71ed8ea31b5c9
Luke Secomb <luke.secomb@live.com>
2025-09-05 00:13:21 +1000

    ✨ vite:css|css-post: add logic to check if CSS is an LWC import or not and handle accordingly. non-LWC CSS imports should be left as is

--- packages/vite-plugin-lwc/src/index.ts
+++ packages/vite-plugin-lwc/src/index.ts
@@ -1,3 +1,6 @@
+import path from "node:path";
+import rollup from "rollup";
+import type { StringFilter, ObjectHook, TransformPluginContext } from "rollup";
 import patch from "./patch.ts";
 import lwc from "./lwc.ts";
 import alias from "./alias.ts";
@@ -5,28 +8,118 @@ import type { Plugin } from "vite";
 import hmr from "./hmr.ts";
 import type { ViteLwcOptions } from "./types.ts";
 import { normalizeOptions } from "./options.ts";
+import { IMPLICIT_DEFAULT_CSS_PATH, IMPLICIT_DEFAULT_HTML_PATH } from "./constants.ts";
+
+function transformOverride(
+  transform: { handler: Function },
+  viteOptions: ViteLwcOptions
+): ObjectHook<(this: TransformPluginContext, code: string, id: string, options?: {
+  ssr?: boolean;
+}) => Promise<rollup.TransformResult> | rollup.TransformResult, {
+  filter?: {
+    id?: StringFilter;
+    code?: StringFilter;
+  };
+}> {
+  // TODO: add support for `npm` modules. Currently relies on `dir|dirs` config
+  const moduleDirs = viteOptions.modules?.reduce<string[]>((acc, item) => {
+    const _item = (item as { dir?: string, dirs?: string })
+    if (_item.dir) {
+      acc.push(_item.dir)
+    }
+    if (_item.dirs) {
+      acc.push(..._item.dirs)
+    }
+
+    return acc
+  }, []) || []
+
+  return {
+    ...transform,
+    async handler(code: string, id: string, options: {
+      ssr?: boolean | undefined;
+    } | undefined) {
+      if (isLwcCss(id, moduleDirs)) {
+        /**
+         * These imports are handled by the LWC engine and
+         * should be ignored by `vite:css`/`vite:css-post`.
+         */
+        return undefined
+      } else {
+        return transform.handler?.call(this, code, id, options)
+      }
+    },
+  }
+}
+
+/**
+ * Simple "is LWC" check. Based on LWC module dir|dirs config passed
+ * to the plugin.
+ *
+ * @param {string} id - Id path to check
+ * @param {string[]} moduleDirs - list of module directories (from dir|dirs config)
+ * @returns {boolean}
+ */
+const isInModuleDirs = (id: string, moduleDirs: string[]) => {
+  const normalizeId = path.normalize(id)
+
+  // loop until a match is found (or fail through all matches)
+  for (const moduleDir of moduleDirs) {
+    // removes the `./` from the prefix so the full path of id matches
+    const normalizeDir = path.normalize(moduleDir)
+    const dirPath = normalizeDir.replace('./', '/')
+
+    if (normalizeId.includes(dirPath)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+const isLwcCss = (id: string, moduleDirs: string[]) => {
+  // should handle '@lwc/resources/empty_html.css' & '@lwc/resources/empty_css.css'
+  if (
+    isInModuleDirs(id, moduleDirs) ||
+    id.startsWith('c/') ||
+    id.includes(IMPLICIT_DEFAULT_HTML_PATH) ||
+    id.includes(IMPLICIT_DEFAULT_CSS_PATH)
+  ) {
+    return true
+  }
+
+  // continue with standard Vite CSS processing
+  return false
+}
+
 
 export default (options: ViteLwcOptions = {}): Plugin[] => {
   options = normalizeOptions(options);
   return [
-  patch({
-    "vite:css": (p) => {
-      p.transform = undefined;
+    patch({
+      "vite:css": (p) => {
+        p.transform = transformOverride(
+          (p.transform as unknown as { handler: Function; }),
+          options
+        );
+      },
+      "vite:css-post": (p) => {
+        p.transform = transformOverride(
+          (p.transform as unknown as { handler: Function; }),
+          options
+        );
+      },
+    }),
+    alias(),
+    {
+      ...lwc(options),
+      apply: "build",
     },
-    "vite:css-post": (p) => {
-      p.transform = undefined;
+    {
+      ...lwc(options),
+      enforce: "post",
+      apply: "serve",
     },
-  }),
-  alias(),
-  {
-    ...lwc(options),
-    apply: "build",
-  },
-  {
-    ...lwc(options),
-    enforce: "post",
-    apply: "serve",
-  },
-  hmr()
-]
+    hmr()
+  ]
 };
